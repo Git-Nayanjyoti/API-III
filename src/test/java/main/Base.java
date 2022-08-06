d1465e4ec5dfe45603515b00dfad77de74446ecb
Nayanjyoti Rabha <njrabha6@gmail.com>
2022-08-06 21:08:23 +0530

    final commit

---
+++ src/test/java/main/Base.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import utils.ConfigRead;
+
+public class Base {
+	String configFilename;
+	Properties configProperties;
+	String currentWorkingDirectory;
+	String uri;
+	Response response;
+
+	public void preSetup() throws IOException {
+		currentWorkingDirectory = System.getProperty("user.dir");
+		configFilename = currentWorkingDirectory + "/src/test/resources/config/config.properties";
+		configProperties = ConfigRead.readConfigProperties(configFilename);
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		LocalDateTime systemTime = LocalDateTime.now();
+		String startTime = dtf.format(systemTime);
+		
+		//Initializing RestAssured api for getting the earthquake data
+		RestAssured.baseURI = configProperties.getProperty("baseUrl");
+		System.out.println(configProperties.getProperty("baseUrl"));
+		uri = "/fdsnws/event/1/query&format=geojson" 
+		        + "&starttime="+ startTime 
+		        + "&minlatitude=" + configProperties.getProperty("minlatitude") 
+		        + "&maxlatitude=" + configProperties.getProperty("maxlatitude") 
+		        + "&minlongitude=" + configProperties.getProperty("minlongitude")
+		        + "&maxlongitude=" + configProperties.getProperty("maxlongitude");
+		
+		response = RestAssured.given().contentType(ContentType.JSON).when().get(uri).then().extract()
+				.response(); 
+	}
+
+}
---
+++ src/test/java/main/GetEarthquakeData.java
@@ -0,0 +1,59 @@
+package main;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.gson.Gson;
+
+import jsonObjects.MainObject;
+import jsonObjects.features;
+
+public class GetEarthquakeData {
+
+	public static void main(String[] args) {
+
+		// for getting the earth quake data after an interval of time
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask() {
+
+			@Override
+			public void run() {
+				try {
+					getEqdata();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		timer.scheduleAtFixedRate(task, 0, 2000);
+	}
+
+	public static void getEqdata() throws IOException {
+		Base base = new Base();
+		base.preSetup();
+
+		// storing the response as a json object
+		String jsonObj = base.response.asPrettyString();
+
+		// regex for finding pune in the title property of the response
+		Pattern pattern = Pattern.compile("(pune|Pune)");
+		Matcher matcher;
+		MainObject mainObject = new Gson().fromJson(jsonObj, MainObject.class);
+		List<features> feature = mainObject.features;
+		for (features property : feature) {
+			matcher = pattern.matcher(property.properties.title);
+			// checking if there is any earthquake in pune to notify the users
+			if (matcher.find() == true) {
+				System.out.println("EarthQuake!!!!");
+			}
+		}
+
+		System.out.println("Successfull!! Restarting again");
+
+	}
+
+}
